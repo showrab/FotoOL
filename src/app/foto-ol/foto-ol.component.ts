1e9496deb2c5a306d745dff3f542d58c2713ca35
showrab <andre@rohrbach.top>
2023-05-15 17:21:41 +0200

    List der Fotos in File foto-list.ts neue "korrekte" Berechnung der Distanz zum Foto. Hint in hint.component.html ausgelagert. Diverse Bereinigungen. Style für Buttons.

--- src/app/foto-ol/foto-ol.component.ts
+++ src/app/foto-ol/foto-ol.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { fotoList } from './foto-list';
 
 @Component({
   selector: 'app-foto-ol',
@@ -6,65 +7,40 @@ import {Component, Input} from '@angular/core';
   styleUrls: ['./foto-ol.component.css']
 })
 export class FotoOlComponent {
-  @Input() teamName: string ='';
+  teamName: string = '';
+
+  readonly fotoList = fotoList;
+  // Index des zu suchenden Fotos
+  index = 0;
+
   // aktuelle Koordinaten mit o,o initialisieren
   center: { lng: number; lat: number; } = {lng: 0, lat: 0};
-
   // Strafpunkte (Summe der Distanz in Meter von den Foto-Koordinaten).
   result = 0;
+  lastDistance = 0;
+
+  //Konfiguration
+  skipMap = true;
+  HINT_PENALTY: number = 10;
 
   // Ablaufsteuerung
   showHelp = true;
-
   hasStarted = false;
   showFotos = false;
   showMap = false;
   showHint = false;
+
   amZiel = false;
 
-  // Index des zu suchenden Fotos
-  index = 0;
-
-  // Fotos mit ihren Koordinaten und Hilfetexten.
-  photos = [
-    {
-      photoUrl: '../../assets/images/Bild.1.jpg',
-      coordinates: {
-        lat: 46.773503,
-        lng: 7.635052
-      },
-      hint: 'Bushaltestelle Ziegeieistrasse'
-    },
-    {
-      photoUrl: '../../assets/images/Bild.2.jpg',
-      coordinates: {
-        lat: 46.774693,
-        lng: 7.636625
-      },
-      hint: 'Obere Ecke Bösbachstrasse'
-    },
-    {
-      photoUrl: '../../assets/images/Bild.3.jpg',
-      coordinates: {
-        lat: 46.775042,
-        lng: 7.634388
-      },
-      hint: 'Bösbach-Finkenweg'
-    },
-    {
-      photoUrl: '../../assets/images/Bild.4.jpg',
-      coordinates: {
-        lat: 46.773958,
-        lng: 7.633619
-      },
-      hint: 'Lin Den Weg'
-    }
-  ];
-  lastDistance = 0;
+  /** wird von HelpComponent.setTeamName() bei jedem Buchstaben vom Input aufgerufen.
+   *
+   * @param teamName
+   */
   setTeamName(teamName: string) {
     this.teamName = teamName;
   }
 
+  /** OL Starten */
   start() {
     if (this.teamName != '') {
       this.showHelp = false;
@@ -73,9 +49,12 @@ export class FotoOlComponent {
     }
   }
 
+  /** URL zum aktuellen Bild. */
   getPhotoUrl() {
-    return (this.photos)[this.index].photoUrl;
+    return (fotoList.photos)[this.index].photoUrl;
   }
+
+  /** Aktuelle Koordinaten speichern. */
   setCurrentCoordinates() {
     navigator.geolocation.getCurrentPosition((position) => {
       this.center = {
@@ -83,40 +62,65 @@ export class FotoOlComponent {
         lng: position.coords.longitude,
       };
       console.log("setCurrentCoordinates center:",this.center);
+
+      if (this.skipMap) {
+        this.nextPhoto();
+      } else {
+        this.showMap = true;
+      }
     });
-    this.showMap = true;
   }
 
+  /** nächstes Foto anzeigen */
   nextPhoto() {
     this.showHint = false;
-    this.lastDistance = this.getDistance();
+    this.lastDistance = this.getDistanceFromLatLonInM();
     this.result += this.lastDistance;
     this.showMap = false;
+
     this.index ++;
-    if (this.index >= this.photos.length) {
+    if (this.index >= fotoList.photos.length) {
       this.amZiel = true;
       this.showFotos = false;
     }
   }
 
+  /** Hilfetext anzeigen
+   * Strafpunkte zum resultat hinzufügen
+   */
   getHint() {
     if (!this.showHint) {
-      this.result += 10
+      this.result += this.HINT_PENALTY;
     }
     this.showHint = true;
-    return (this.photos)[this.index].hint;
+    return (fotoList.photos)[this.index].hint;
   }
 
-  getDistance(): number {
-    const x1 = this.center?.lat;
-    const x2 = this.photos[this.index].coordinates.lat;
-    const y1 = this.center?.lng;
-    const y2 = this.photos[this.index].coordinates.lng;
+  /** Bereschne die Distanz von deinem Standpunkt zu dem Foto
+   *
+   */
+  getDistanceFromLatLonInM() {
+    const r = 6373.0;
+    const lat1 = this.center?.lat;
+    const lat2 = fotoList.photos[this.index].coordinates.lat;
+    const lon1 = this.center?.lng;
+    const lon2 = fotoList.photos[this.index].coordinates.lng;
 
-    const a = x1 - x2;
-    const b = y1 - y2;
-    const c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
-
-    return Math.trunc(c * 1000); //in Meter umrechnen
+    const R = 6371; // Radius of the earth in km
+    const dLat = this.deg2rad(lat2 - lat1);  // deg2rad below
+    const dLon = this.deg2rad(lon2 - lon1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    ;
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c; // Distance in km
+    return Math.trunc(d * 1000);
   }
+
+  deg2rad(deg: number) {
+    return deg * (Math.PI/180)
+  }
+
 }
--- src/app/hint/hint.component.ts
+++ src/app/hint/hint.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-hint',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./hint.component.css']
 })
 export class HintComponent {
-
+  @Input() hintText: any;
 }
