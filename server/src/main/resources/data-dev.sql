fe716e1281fb2009d92d948f09bfc2d209a1550a
X-and-R <xander.kaempfer@hotmail.com>
2023-06-02 21:02:34 +0200

    Koordinaten zurückgetauscht

--- server/src/main/resources/data-dev.sql
+++ server/src/main/resources/data-dev.sql
@@ -14,113 +14,113 @@ values ('test', false, 4, '../../assets/images/Bild.4.jpg', 46.773958, 7.633619,
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
 values ('andre', false, 1, '../../assets/images/lucas.jpeg', 46.773958, 7.633619, 'Schallenberg');
 
--- Parcours "Üben"
+-- Parcours "Ueben"
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Üben', false, 1, '../../assets/images/u.1.0339', 7.63025, 46.76203, 'Neben dem Haus');
+values ('Üben', false, 1, '../../assets/images/u.1.0339', 46.76203, 7.63025, 'Neben dem Haus');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Üben', false, 2, '../../assets/images/u.2.5429', 7.63049, 46.76196, 'Hinter dem Haus');
+values ('Üben', false, 2, '../../assets/images/u.2.5429', 46.76196, 7.63049, 'Hinter dem Haus');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Üben', false, 3, '../../assets/images/u.3.0338', 7.63022, 46.76219, 'Vor dem Haus');
+values ('Üben', false, 3, '../../assets/images/u.3.0338', 46.76219, 7.63022, 'Vor dem Haus');
 
 -- Parcours "Einfach"
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Einfach', false, 1, '../../assets/images/e.1.5308', 7.630812, 46.762633, 'Richtung Steffisburg');
+values ('Einfach', false, 1, '../../assets/images/e.1.5308', 46.762633, 7.630812, 'Richtung Steffisburg');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Einfach', false, 2, '../../assets/images/e.2.5311', 7.63085, 46.76241, 'Krankenhausstrasse');
+values ('Einfach', false, 2, '../../assets/images/e.2.5311', 46.76241, 7.63085, 'Krankenhausstrasse');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Einfach', false, 3, '../../assets/images/e.3.0263', 7.63085, 46.76241, 'Kramnkenhausstrasse');
+values ('Einfach', false, 3, '../../assets/images/e.3.0263', 46.76241, 7.63085, 'Kramnkenhausstrasse');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Einfach', false, 4, '../../assets/images/e.4.0283', 7.63203, 46.76058, 'Spital Eingang');
+values ('Einfach', false, 4, '../../assets/images/e.4.0283', 46.76058, 7.63203, 'Spital Eingang');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Einfach', false, 5, '../../assets/images/e.5.0287', 7.63145, 46.76036, 'Tankstelle');
+values ('Einfach', false, 5, '../../assets/images/e.5.0287', 46.76036, 7.63145, 'Tankstelle');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Einfach', false, 6, '../../assets/images/e.6.5407', 7.62944, 46.76120, 'Berntorkreisel');
+values ('Einfach', false, 6, '../../assets/images/e.6.5407', 46.76120, 7.62944, 'Berntorkreisel');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Einfach', false, 7, '../../assets/images/e.7.0336', 7.62974, 46.76177, 'Steffisburgstrasse');
+values ('Einfach', false, 7, '../../assets/images/e.7.0336', 46.76177, 7.62974, 'Steffisburgstrasse');
 
 -- Parcours "Schwierig"
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 1, '../../assets/images/Sch.1.0259', 7.63048, 46.76244, 'Steffisburgstrasse');
+values ('Schwierig', false, 1, '../../assets/images/Sch.1.0259', 46.76244, 7.63048, 'Steffisburgstrasse');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 2, '../../assets/images/Sch.2.0261', 7.63079, 46.76276, 'Goldiwilstrasse');
+values ('Schwierig', false, 2, '../../assets/images/Sch.2.0261', 46.76276, 7.63079, 'Goldiwilstrasse');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 3, '../../assets/images/Sch.3.0270', 7.63180, 46.76157, 'Spital');
+values ('Schwierig', false, 3, '../../assets/images/Sch.3.0270', 46.76157, 7.63180, 'Spital');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 4, '../../assets/images/Sch.4.0271', 7.63193, 46.76182, 'Spital');
+values ('Schwierig', false, 4, '../../assets/images/Sch.4.0271', 46.76182, 7.63193, 'Spital');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 5, '../../assets/images/Sch.5.0269', 7.63211, 46.76175, 'Spital');
+values ('Schwierig', false, 5, '../../assets/images/Sch.5.0269', 46.76175, 7.63211, 'Spital');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 6, '../../assets/images/Sch.6.0275', 7.63331, 46.76172, 'hinterm Spital');
+values ('Schwierig', false, 6, '../../assets/images/Sch.6.0275', 46.76172, 7.63331, 'hinterm Spital');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 7, '../../assets/images/Sch.7.0276', 7.63344, 46.76145, 'altes Spital');
+values ('Schwierig', false, 7, '../../assets/images/Sch.7.0276', 46.76145, 7.63344, 'altes Spital');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 8, '../../assets/images/Sch.8.0277', 7.63356, 46.76101, 'Spital');
+values ('Schwierig', false, 8, '../../assets/images/Sch.8.0277', 46.76101, 7.63356, 'Spital');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 9, '../../assets/images/Sch.9.0278', 7.63351, 46.76077, 'Spital Sonnseite');
+values ('Schwierig', false, 9, '../../assets/images/Sch.9.0278', 46.76077, 7.63351, 'Spital Sonnseite');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 10, '../../assets/images/Sch.10.0284', 7.63161, 46.76100, 'Spital');
+values ('Schwierig', false, 10, '../../assets/images/Sch.10.0284', 46.76100, 7.63161, 'Spital');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 11, '../../assets/images/Sch.11.0288', 7.63155, 46.,76000, 'Parkhaus');
+values ('Schwierig', false, 11, '../../assets/images/Sch.11.0288', 46.,76000, 7.63155, 'Parkhaus');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 12, '../../assets/images/Sch.12.0295', 7.63230, 46.75904, 'Schlossberg Süd');
+values ('Schwierig', false, 12, '../../assets/images/Sch.12.0295', 46.75904, 7.63230, 'Schlossberg Süd');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 13, '../../assets/images/Sch.13.0300', 7.63177, 46.75847, 'Schlossberg Süd');
+values ('Schwierig', false, 13, '../../assets/images/Sch.13.0300', 46.75847, 7.63177, 'Schlossberg Süd');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 14, '../../assets/images/Sch.14.0304', 7.63014, 46.75927, 'von Nordwest');
+values ('Schwierig', false, 14, '../../assets/images/Sch.14.0304', 46.75927, 7.63014, 'von Nordwest');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 15, '../../assets/images/Sch.15.0309', 7.62822, 46.75901, 'Mühleplatz');
+values ('Schwierig', false, 15, '../../assets/images/Sch.15.0309', 46.75901, 7.62822, 'Mühleplatz');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 16, '../../assets/images/Sch.16.0312', 7.62794, 46.75883, 'Brüggli');
+values ('Schwierig', false, 16, '../../assets/images/Sch.16.0312', 46.75883, 7.62794, 'Brüggli');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 17, '../../assets/images/Sch.17.0315', 7.62721, 46.75879, 'Bälliz');
+values ('Schwierig', false, 17, '../../assets/images/Sch.17.0315', 46.75879, 7.62721, 'Bälliz');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 18, '../../assets/images/Sch.18.5396', 7.62690, 46.75913, 'auch Bälliz');
+values ('Schwierig', false, 18, '../../assets/images/Sch.18.5396', 46.75913, 7.62690, 'auch Bälliz');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 19, '../../assets/images/Sch.19.0318', 7.62659, 46.76030, 'an der Aare');
+values ('Schwierig', false, 19, '../../assets/images/Sch.19.0318', 46.76030, 7.62659, 'an der Aare');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 20, '../../assets/images/Sch.20.0320', 7.62577, 46.76054, 'Scheibenstrasse');
+values ('Schwierig', false, 20, '../../assets/images/Sch.20.0320', 46.76054, 7.62577, 'Scheibenstrasse');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 21, '../../assets/images/Sch.21.0327', 7.62682, 46.76164, 'Grabengut');
+values ('Schwierig', false, 21, '../../assets/images/Sch.21.0327', 46.76164, 7.62682, 'Grabengut');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 22, '../../assets/images/Sch.22.0331', 7.62825, 46.76143, 'Grabengut');
+values ('Schwierig', false, 22, '../../assets/images/Sch.22.0331', 46.76143, 7.62825, 'Grabengut');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 23, '../../assets/images/Sch.23.0334', 7.62953, 46.76159, 'Burgzentrum');
+values ('Schwierig', false, 23, '../../assets/images/Sch.23.0334', 46.76159, 7.62953, 'Burgzentrum');
 
 insert into photo (tour_name, hidden, sort_order, photo_url, lat, lng, hint)
-values ('Schwierig', false, 24, '../../assets/images/Sch.24.0336', 7.62974, 46.76177, 'Steffisburgstrasse');
+values ('Schwierig', false, 24, '../../assets/images/Sch.24.0336', 46.76177, 7.62974, 'Steffisburgstrasse');
 
 
 -- Tabelle High Score
