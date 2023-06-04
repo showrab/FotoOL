2eda6e5386f2ff8b09c7bd07e3b0aac36b51b701
showrab <a.rohrbach@gmx.net>
2023-06-04 22:53:17 +0200

    High Scores werden automatisch alle 30s neu geladen

--- client/src/app/ziel/ziel.component.ts
+++ client/src/app/ziel/ziel.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {FotoOlService} from "../foto-ol.service";
 import {HighScore} from "../model/high-score";
+import {Observable, startWith, Subject, Subscription, switchMap, timer} from "rxjs";
 
 @Component({
   selector: 'app-ziel',
@@ -12,11 +13,28 @@ export class ZielComponent implements OnInit {
   @Input() tourName: string | undefined;
 
   highScores: any;
+  private reset$ = new Subject();
+  timer$: Observable<any>;
+  subscription: Subscription | undefined;
 
   constructor(private fotoOlService: FotoOlService) {
+    //Timer von 30s starten
+    this.timer$ = this.reset$.pipe(
+      startWith(0),
+      switchMap(() => timer(0, 30000))
+    );
   }
   ngOnInit() {
     this.loadHighScore();
+    // Timer wird alle 30s aufgerufen
+    this.subscription = this.timer$.subscribe((i) => {
+      this.loadHighScore();
+    });
+  }
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
   loadHighScore() {
