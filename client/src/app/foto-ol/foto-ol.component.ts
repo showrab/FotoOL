0409a77c3a4e82800c1f542a7c52fba057ab017b
showrab <a.rohrbach@gmx.net>
2023-06-06 23:04:07 +0200

    Distanz und Richtung zu nächstem Foto, hintPenalty = 50m

--- client/src/app/foto-ol/foto-ol.component.ts
+++ client/src/app/foto-ol/foto-ol.component.ts
@@ -9,7 +9,6 @@ import { NgxSpinnerService } from 'ngx-spinner';
 export enum FotoOlState {
   showHelp = 'showHelp',
   showFotos = 'showFotos',
-  amZiel = 'amZiel',
   showHighScore ='showHighScore',
   showAllHighScore = 'showAllHighScore',
   showAdmin = 'showAdmin'
@@ -27,10 +26,13 @@ export class FotoOlComponent implements OnInit{
   center: Center = {lng: 0, lat: 0};
   //letzte Distanz separat ausweisen
   lastDistance = 0;
+  //Distanz und Richtung
+  distanzRichtung: string = '';
+  richtung: string = '';
 
   //Konfiguration
   skipMap = true;
-  hintPenalty: number = 10;
+  hintPenalty: number = 50;
 
   // Ablaufsteuerung
   FotoOlState = FotoOlState;
@@ -59,28 +61,37 @@ export class FotoOlComponent implements OnInit{
   init() {
     this.loadTeamList();
     this.loadLocalStorrage();
-  }
-
-  loadPhotos(tourName: string) {
-    this.fotoOlService.findAllByTour(tourName).subscribe(photos => {
-      this.photos = photos;
-    });
+    this.setCurrentCoordinates(true);
   }
 
   loadTeamList() {
     this.fotoOlService.getTour().subscribe((tourList)=> {
       this.tourList = tourList;
-      //this.loadPhotos();
     })
   }
 
+  loadPhotos(tourName: string) {
+    console.log("loadPhotos(%s)", tourName);
+    this.fotoOlService.findAllByTour(tourName).subscribe(photos => {
+      this.photos = photos;
+      //Koordinaten laden um distanz und Richtung zu berechnen
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        this.getDistanceFromLatLonInM(this.center);
+      });
+    });
+  }
+
   loadLocalStorrage() {
-    console.log('Lokal gespeicherte Werte laden');
+    console.log('loadLocalStorrage() Lokal gespeicherte Werte laden');
     //------ zuerst tourName laden --------
     let tourName = localStorage.getItem('tourName')
     if (tourName) {
       this.score.tourName = tourName;
-      console.log('  load tourName=', tourName);
+      //console.log('  load tourName=', tourName);
       this.loadPhotos(tourName);
       this.state = FotoOlState.showFotos;
       this.showMap = false;
@@ -90,31 +101,31 @@ export class FotoOlComponent implements OnInit{
     let teamName = localStorage.getItem('teamName')
     if (teamName) {
       this.score.teamName = teamName;
-      console.log('  load teamName=', this.score.teamName);
+      //console.log('  load teamName=', this.score.teamName);
       this.fotoOlService.findTeamName(teamName).subscribe( (team) => {
         this.myTourScoreList = team;
-        console.log('  loaded tourList.lenght: ', team.length);
-        console.log('  loaded tourList[0]: ', team[0]);
+        //console.log('  loaded tourList.lenght: ', team.length);
+        //console.log('  loaded tourList[0]: ', team[0]);
         let myTour = team.find( team => {
           return team.tourName == tourName;
         })
-        console.log('  loaded tour: ', myTour);
+        //console.log('  loaded tour: ', myTour);
         if (myTour) {
           this.score = myTour;
         } else {
           this.score.score = 0;
         }
-        console.log('  score:', this.score);
+        //console.log('  score:', this.score);
 
         //weitere gespeicherte Werte laden
         //-------------
         let index = localStorage.getItem('index');
         if (index) this.score.index = Number(index);
-        console.log('  load index=', this.score.index);
+        //console.log('  load index=', this.score.index);
         //-------------
         let scoreId = localStorage.getItem('scoreId');
         if (scoreId) this.score.id = Number(scoreId);
-        console.log('  load scoreId=', this.score.id);
+        //console.log('  load scoreId=', this.score.id);
         //-------------
         let lastDistance = localStorage.getItem('lastDistance');
         if (lastDistance) {
@@ -160,19 +171,22 @@ export class FotoOlComponent implements OnInit{
   }
 
   /** Aktuelle Koordinaten speichern. */
-  setCurrentCoordinates() {
+  setCurrentCoordinates(init: boolean) {
     this.spinner.show();
     navigator.geolocation.getCurrentPosition((position) => {
       this.center = {
         lat: position.coords.latitude,
         lng: position.coords.longitude,
       };
-//      console.log("setCurrentCoordinates center:",this.center);
-
-      if (this.skipMap) {
-        this.nextPhoto();
+      //console.log("setCurrentCoordinates center:",this.center);
+      if (init) {
+        console.log("Ich starte hier: %0.6f, %0.6f", this.center.lat, this.center.lng);
       } else {
-        this.showMap = true;
+        if (this.skipMap) {
+          this.nextPhoto();
+        } else {
+          this.showMap = true;
+        }
       }
     });
   }
@@ -182,8 +196,10 @@ export class FotoOlComponent implements OnInit{
     this.spinner.show();
     localStorage.setItem('index', String(this.score.index+1));
 
-    this.lastDistance = this.getDistanceFromLatLonInM();
+    //Berechne Distanz zu aktuellem Foto
+    this.lastDistance = this.getDistanceFromLatLonInM(this.center);
     localStorage.setItem('lastDistance', String(this.lastDistance));
+    console.log("meine Koordinaten: %0.6f, %0.6f bei Bild %d. Distanz %dm", this.center.lat, this.center.lng, this.score.index+1, this.lastDistance);
 
     this.score.score += this.lastDistance;
 
@@ -210,7 +226,11 @@ export class FotoOlComponent implements OnInit{
         localStorage.removeItem('scoreId');
         localStorage.removeItem('lastDistance');
         this.spinner.hide();
+        return;
       }
+      //Berechne die Distanz und Richtung zu neuem Foto
+      this.getDistanceFromLatLonInM(this.center);
+      console.log("Distanz %s zu Bild %d", this.distanzRichtung, this.score.index+1);
       console.log("index: %d, anzPhotos: %d", this.score.index, this.photos?.length)
     });
 
@@ -238,12 +258,13 @@ export class FotoOlComponent implements OnInit{
   /** Bereschne die Distanz von deinem Standpunkt zu dem Foto
    * @return distanz in Meter
    */
-  getDistanceFromLatLonInM() {
-    const r = 6373.0;
-    const lat1 = this.center?.lat;
+  getDistanceFromLatLonInM(center: Center) {
+    //const r = 6373.0;
+    const lat1 = center.lat;
     const lat2 = this.photos?this.photos[this.score.index].lat:0;
-    const lon1 = this.center?.lng;
+    const lon1 = center.lng;
     const lon2 = this.photos?this.photos[this.score.index].lng:0;
+    console.log("Koordinate Bild %s: %0.6f, %0.6f", this.score.index+1, this.photos?this.photos[this.score.index].lat:0, this.photos?this.photos[this.score.index].lng:0)
 
     const R = 6371; // Radius of the earth in km
     const dLat = this.deg2rad(lat2 - lat1);  // deg2rad below
@@ -255,7 +276,16 @@ export class FotoOlComponent implements OnInit{
     ;
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     const d = R * c; // Distance in km
-    return Math.trunc(d * 1000);
+
+    let distanz: number = Math.trunc(d * 1000);
+
+    //Richtung
+    //console.log("dLat: %f / dLon: %f = %f", lat2-lat1, lon2-lon1, (lat2-lat1) / (lon2-lon1));
+    //console.log("dLat: %f, dLon: %f = %f", dLat, dLon, dLat/dLon);
+    this.richtung = this.getRichtung(dLon, dLat);
+    this.distanzRichtung = distanz + "m in Richtung " + this.richtung;
+
+    return distanz;
   }
 
   deg2rad(deg: number) {
@@ -291,4 +321,37 @@ export class FotoOlComponent implements OnInit{
     this.sepChar = ''
     this.lastDistance = 0;
   }
+
+  getRichtung(x: number, y: number): string {
+    let rad = Math.atan(x/y);
+    //console.log("Richtung rad", rad);
+
+    let grad = rad * (180 / Math.PI);
+    if (y < 0 && x >= 0) grad += 180;
+    if (y < 0 && x < 0) grad += 180;
+    if (y >= 0 && x < 0) grad += 360;
+
+    //console.log("Richtung grad", grad);
+    let pm = 11.25;
+    if (grad > 0 - pm && grad <= 0 + pm) return "Nord N";
+    if (grad > 22.5 - pm && grad <= 22.5 + pm) return "Nordnordost";
+    if (grad > 45 - pm && grad <= 45 + pm) return "Nordost";
+    if (grad > 67.5 - pm && grad <= 67.5 + pm) return "Ostnordost";
+    if (grad > 90 - pm && grad <= 90 + pm) return "Osten";
+    if (grad > 112.5 - pm && grad <= 112.5 + pm) return "Ostsüdost";
+    if (grad > 135 - pm && grad <= 135 + pm) return "Südost";
+    if (grad > 157.5 - pm && grad <= 157.5 + pm) return "Südsüdost";
+    if (grad > 180 - pm && grad <= 180 + pm) return "Süden";
+    if (grad > 202.5 - pm && grad <= 202.5 + pm) return "Südsüdwest";
+    if (grad > 225 - pm && grad <= 225 + pm) return "Südwest";
+    if (grad > 247.5 - pm && grad <= 247.5 + pm) return "Westsüdwest";
+    if (grad > 270 - pm && grad <= 270 + pm) return "Westen";
+    if (grad > 292.5 - pm && grad <= 292.5 + pm) return "Westnordwest";
+    if (grad > 315 - pm && grad <= 315 + pm) return "Nordwest";
+    if (grad > 337.5 - pm && grad <= 337.5 + pm) return "Nordnordwest";
+    return grad + '°';
+  }
+
+  protected readonly Math = Math;
+
 }
--- client/src/app/test/test.component.ts
+++ client/src/app/test/test.component.ts
@@ -9,5 +9,40 @@ import { Component } from '@angular/core';
 export class TestComponent {
   lat: number = 51.678418;
   lng: number = 7.809007;
+
+
+
+
+  getRichtung(b: number, a: number): string {
+    let rad = Math.atan(b/a);
+    console.log("Richtung rad", rad);
+
+    let grad = rad * (180 / Math.PI);
+    if (a < 0 && b >= 0) grad += 180;
+    if (a < 0 && b < 0) grad += 180;
+    if (a >= 0 && b < 0) grad += 360;
+
+    console.log("Richtung grad", grad);
+    let pm = 11.25;
+    if (grad > 0 - pm && grad <= 0 + pm) return "Nord N";
+    if (grad > 22.5 - pm && grad <= 22.5 + pm) return "Nordnordost NNO";
+    if (grad > 45 - pm && grad <= 45 + pm) return "Nordost NO";
+    if (grad > 67.5 - pm && grad <= 67.5 + pm) return "Ostnordost ONO";
+    if (grad > 90 - pm && grad <= 90 + pm) return "Osten O";
+    if (grad > 112.5 - pm && grad <= 112.5 + pm) return "Ostsüdost OSO";
+    if (grad > 135 - pm && grad <= 135 + pm) return "Südost SO";
+    if (grad > 157.5 - pm && grad <= 157.5 + pm) return "Südsüdost	SSO";
+    if (grad > 180 - pm && grad <= 180 + pm) return "Süden	S";
+    if (grad > 202.5 - pm && grad <= 202.5 + pm) return "Südsüdwest	SSW";
+    if (grad > 225 - pm && grad <= 225 + pm) return "Südwest	SW";
+    if (grad > 247.5 - pm && grad <= 247.5 + pm) return "Westsüdwest	WSW";
+    if (grad > 270 - pm && grad <= 270 + pm) return "Westen	W";
+    if (grad > 292.5 - pm && grad <= 292.5 + pm) return "Westnordwest	WNW";
+    if (grad > 315 - pm && grad <= 315 + pm) return "Nordwest	NW";
+    if (grad > 337.5 - pm && grad <= 337.5 + pm) return "Nordnordwest	NNW";
+    return grad + '°';
+  }
+
+  protected readonly Math = Math;
 }
 
